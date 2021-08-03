2fa9a7714b06f12c3eb2f73936f05ffc2a4dcbf2
huy <huy@gmail.com>
2021-08-03 18:57:46 +0700

    ok

--- routes/torrent.js
+++ routes/torrent.js
@@ -5,30 +5,43 @@ const WebTorrent = require("webtorrent");
 const path = require("path");
 const fs = require("fs");
 
-router.post("/", uploadMiddelware.single("file"), async (req, res) => {
-  let pathname = path.join(__dirname, "..", "public");
-  const client = new WebTorrent();
-  console.log("waiting....................");
-  client.add(req.file.path, { path: pathname }, function (torrent) {
-    torrent.on("download", function (bytes) {
-      console.log("just downloaded: " + bytes);
-      console.log("total downloaded: " + torrent.downloaded);
-      console.log("download speed: " + torrent.downloadSpeed);
-      console.log("progress: " + torrent.progress);
-      console.log((torrent.progress * 100).toFixed());
-    });
-    torrent.on("done", function () {
-      fs.writeFile(pathname + `/${req.file.originalname}.txt`, "", (err) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        //file written successfully
+router.post("/", uploadMiddelware.single("file"), async (req, res, next) => {
+  let handler;
+  try {
+    handler = setInterval(() => {
+      axios
+        .get(`https://${process.env.domain}.herokuapp.com/api/vlluon`)
+        .then((re) => console.log("wake", re.data));
+    }, 25 * 60 * 1000);
+    let pathname = path.join(__dirname, "..", "public");
+    const client = new WebTorrent();
+    console.log("waiting....................");
+    client.add(req.file.path, { path: pathname }, function (torrent) {
+      torrent.on("download", function (bytes) {
+        console.log("just downloaded: " + bytes);
+        console.log("total downloaded: " + torrent.downloaded);
+        console.log("download speed: " + torrent.downloadSpeed);
+        console.log("progress: " + torrent.progress);
+        console.log((torrent.progress * 100).toFixed());
       });
-      console.log("torrent downsload finished");
+      torrent.on("done", function () {
+        fs.writeFile(pathname + `/${req.file.originalname}.txt`, "", (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          //file written successfully
+        });
+        clearInterval(handler);
+        console.log("torrent downsload finished");
+      });
     });
-  });
-  res.json(req.file.filename);
+    res.json(req.file.filename);
+  } catch {
+    clearInterval(handler);
+    console.log("torrent clear");
+    next();
+  }
 });
 
 module.exports = router;
