3611254d875c6818e65eecaaa7e6bfe993a96475
huy <huy@gmail.com>
2021-08-07 19:37:23 +0700

    ok

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(
   serveIndex("public", { icons: true })
 );
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: false, limit: "5mb" }));
 
 //routes
